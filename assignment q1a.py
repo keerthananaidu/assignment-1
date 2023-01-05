db33e0086f9e846b6a0e7a28af1376c2fd2eb22c
Keerthana <121809912+keerthananaidu@users.noreply.github.com>
2023-01-05 17:37:44 +0530

    Add files via upload
    sum of two numbers with user input

---
+++ assignment q1a.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+number1 = float (input("Number 1:"))
+
+number2 = float (input("Number 2:"))
+
+sum = number1 + number2
+
+print("sum:", sum)
+
+
